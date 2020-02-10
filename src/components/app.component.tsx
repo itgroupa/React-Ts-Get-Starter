b26cc610c8805ba0ae47dd6a28099482087d9139
alexey.tyurin <alexey.tyurin@kaspersky.com>
2020-02-10 11:04:23 +0300

    +

---
+++ src/components/app.component.tsx
@@ -0,0 +1,8 @@
+  
+import * as React from 'react';
+
+export const App: React.FC<{}> = () => {
+  return (
+      <h1>React Redux Typescript</h1>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { App } from './components/app.component';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById("root")
+);
